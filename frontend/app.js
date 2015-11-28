b3f8f331fd8b8bebd736c8927aab397ec901474b
TheConnMan <bcconn2112@gmail.com>
2015-11-28 15:56:08 -0500

    Move static files to public folder

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.sendFile('app/index.html', {root: __dirname })
-});
+app.use(express.static('public'));
 
 var server = app.listen(3000, function () {
 	var host = server.address().address;
