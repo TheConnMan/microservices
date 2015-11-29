cf557a2f817be7fe9391cd058056a55470ccb8a3
TheConnMan <bcconn2112@gmail.com>
2015-11-29 00:16:55 -0500

    Expose rest server for testing

--- poor-mans-docker-compose.sh
+++ poor-mans-docker-compose.sh
@@ -8,6 +8,6 @@ docker kill frontend worker rest-server rabbitmq
 docker rm frontend worker rest-server rabbitmq
 
 docker run -d -p 5672:5672 --name=rabbitmq rabbitmq
-docker run -d --name=rest-server microservices/rest-server
+docker run -d -p 8080:8080 --name=rest-server microservices/rest-server
 docker run -d --link rabbitmq --link rest-server --name=worker microservices/worker
 docker run -d -p 80:3000 --link rabbitmq --link rest-server --name=frontend microservices/frontend
