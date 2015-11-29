53d4408f83836fe9edcb3e22c70441f46624066b
TheConnMan <bcconn2112@gmail.com>
2015-11-28 21:52:13 -0500

    Add worker to docker compose

--- poor-mans-docker-compose.sh
+++ poor-mans-docker-compose.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 docker build -t microservices/frontend ./frontend/
+docker build -t microservices/worker ./worker/
 
-docker kill frontend rabbitmq
-docker rm frontend rabbitmq
+docker kill frontend worker rabbitmq
+docker rm frontend worker rabbitmq
 
 docker run -d -p 5672:5672 --name=rabbitmq rabbitmq
+docker run -d --link rabbitmq --name=worker microservices/worker
 docker run -d -p 80:3000 --link rabbitmq --name=frontend microservices/frontend
